84103a299f86d28e38c4e63096805b50ee80fcaa
codewithash <402308182@my.richfield.ac.za>
2025-04-15 10:49:05 +0200

    Display address project

---
+++ displayAddress/src/displayAddress/First.java
@@ -0,0 +1,16 @@
+package displayAddress;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		First.displayAddress();
+	}
+	
+	public static void displayAddress() {
+		System.out.println("XYZ Company");
+		System.out.println("8900 U.S Hwy 14");
+		System.out.println("Crystal Lake, IL 600141");
+	}
+
+}
